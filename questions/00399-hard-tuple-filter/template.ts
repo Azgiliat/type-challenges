522c5e25ddacd62c53e895d93d16082704f7a701
azgiliat <toxa2088@gmail.com>
2023-10-16 23:00:20 +0300

    tuple filter

--- questions/00399-hard-tuple-filter/template.ts
+++ questions/00399-hard-tuple-filter/template.ts
@@ -1,1 +1,6 @@
-type FilterOut<T extends any[], F> = any
+type FilterOut<Source extends any[], FilterValue, Result extends any[] = []> = Source extends [infer R, ...infer Rest]
+  ? [R] extends [FilterValue]
+      ? FilterOut<Rest, FilterValue, Result> : FilterOut<Rest, FilterValue, [...Result, R]>
+  : [...Result]
+
+// type FilterOut<T extends any[], F> = any
