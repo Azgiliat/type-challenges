4cf43fec8d40a8a803c3abefc42b4fc50bee11c8
azgiliat <toxa2088@gmail.com>
2023-10-18 01:22:07 +0300

    med string to union

--- questions/00531-medium-string-to-union/template.ts
+++ questions/00531-medium-string-to-union/template.ts
@@ -1,1 +1,1 @@
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string, Result extends string = ''> = T extends `${infer R}${infer Rest}` ? StringToUnion<Rest, R | Result> : Exclude<Result, ''>
